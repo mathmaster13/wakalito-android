e1827d4fcbe626ae62dd5bc114c00741f5f6417b
mathmaster13 <>
2024-06-28 23:49:12 -0400

    i would REALLY like to remove all dependencies on jetpack compose. i don't want to use it. starting work on the real app

--- app/src/main/java/io/github/mathmaster13/wakalito/MainActivity.kt
+++ app/src/main/java/io/github/mathmaster13/wakalito/MainActivity.kt
@@ -3,47 +3,11 @@ package io.github.mathmaster13.wakalito
 // TODO make the credits and table of symbols like iOS
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import io.github.mathmaster13.wakalito.ui.theme.NasinSitelenWakalitoTheme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            NasinSitelenWakalitoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                            name = "friends",
-                            modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name! This part does not exist yet.",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NasinSitelenWakalitoTheme {
-        Greeting("Android")
+        setContentView(R.layout.app)
     }
 }
